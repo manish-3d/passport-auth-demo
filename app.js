4d73117975ca1418c52b40b5c1917c90e702d845
manish-3d <manishkumar.intc@gmail.com>
2026-05-25 20:20:21 +0530

    app configuration done

--- app.js
+++ app.js
@@ -70,3 +70,35 @@ passport.deserializeUser(
   }
 );
 
+/* ---------------- APP CONFIG ---------------- */
+
+app.set(
+  "views",
+  path.join(__dirname, "views")
+);
+
+app.set(
+  "view engine",
+  "ejs"
+);
+
+app.use(
+  session({
+    secret: "cats",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+
+app.use(passport.session());
+
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
+
+/* ---------------- ROUTES ---------------- */
+
