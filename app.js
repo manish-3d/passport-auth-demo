f0696c060f13da96a81a4fce9b4a2ca646fc64af
manish-3d <manishkumar.intc@gmail.com>
2026-05-25 20:21:06 +0530

    routes added in app.js

--- app.js
+++ app.js
@@ -102,3 +102,78 @@ app.use(
 
 /* ---------------- ROUTES ---------------- */
 
+app.get("/", (req, res) => {
+  res.render("index", {
+    user: req.user,
+  });
+});
+
+app.get("/sign-up", (req, res) => {
+  res.render("sign-up-form");
+});
+
+app.post(
+  "/sign-up",
+  async (req, res, next) => {
+    try {
+      const hashedPassword =
+        await bcrypt.hash(
+          req.body.password,
+          10
+        );
+
+      await pool.query(
+        `
+        INSERT INTO users
+        (username,password)
+        VALUES ($1,$2)
+        `,
+        [
+          req.body.username,
+          hashedPassword,
+        ]
+      );
+
+      res.redirect("/");
+    } catch (err) {
+      next(err);
+    }
+  }
+);
+
+app.post(
+  "/log-in",
+  passport.authenticate(
+    "local",
+    {
+      successRedirect: "/",
+      failureRedirect: "/",
+    }
+  )
+);
+
+app.get(
+  "/log-out",
+  (req, res, next) => {
+    req.logout((err) => {
+      if (err) {
+        return next(err);
+      }
+
+      res.redirect("/");
+    });
+  }
+);
+
+app.get(
+  "/check-user",
+  (req, res) => {
+    res.json(req.user || null);
+  }
+);
+
+app.listen(3000, () => {
+  console.log(
+    "Server running on port 3000"
+  );
+});
